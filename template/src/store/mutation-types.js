2fa8ff234eb929c7b76b280465ce0d5d4ffcbd9c
Jason Ihaia <jihaia@Jasons-MacBook-Pro.local>
2018-01-01 16:41:03 -0700

    added vuex support

---
+++ template/src/store/mutation-types.js
@@ -0,0 +1,29 @@
+// Layout
+export const CLOSE_CHOOSER = 'CLOSE_CHOOSER'
+export const TOGGLE_CHOOSER = 'TOGGLE_CHOOSER'
+export const OPEN_SETTINGS = 'OPEN_SETTINGS'
+export const CLOSE_SETTINGS = 'CLOSE_SETTINGS'
+
+// Navigation
+export const CLOSE_NAVIGATOR = 'CLOSE_NAVIGATOR'
+export const TOGGLE_NAVIGATOR = 'TOGGLE_NAVIGATOR'
+export const SET_TITLE = 'SET_TITLE'
+export const RESET_TITLE = 'RESET_TITLE'
+
+export const SET_ALL_QUOTES = 'SET_ALL_QUOTES'
+export const SET_TEMPLATES = 'SET_TEMPLATES'
+export const SET_DOWNLOADS = 'SET_DOWNLOADS'
+export const SET_CHECK_STATUS = 'SET_CHECK_STATUS'
+export const SET_GENERIC = 'SET_GENERIC'
+
+// Security
+export const LOG_IN = 'LOG_IN'
+export const LOG_OUT = 'LOG_OUT'
+
+// Notifications
+export const SHOW_SNACKBAR = 'SHOW_SNACKBAR'
+export const HIDE_SNACKBAR = 'HIDE_SNACKBAR'
+
+// Quotes
+export const SET_QUOTE = 'SET_QUOTE'
+export const CLEAR_QUOTE = 'CLEAR_QUOTE'
